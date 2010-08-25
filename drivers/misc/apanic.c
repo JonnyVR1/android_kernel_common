e0520774f2fe60fa6b0ab60329cd5d3f6bebc9ff
Todd Poynor <toddpoynor@google.com>
2010-08-24 18:10:35 -0700

    apanic common/MTD split:
       - MTD code moved to separate file and separately selected in config.
    
       - MTD-specific code calls common apanic_register_device() to
         inform a panic dump device is ready (and tell block transfer size).
    
       - Common code calls canned externals apanic_dev_read(),
         apanic_dev_write(), apanic_dev_erase().
    
       - Ref to ram_console_enable_console() protected with
         #ifdef CONFIG_ANDROID_RAM_CONSOLE.
    
       - Add NULL return check for  __get_free_page() call.
    
    Change-Id: Ifc9f522d8533b00fcb143424aeeecffda0a59fb8
    Signed-off-by: Todd Poynor <toddpoynor@google.com>

--- drivers/misc/apanic.c
+++ drivers/misc/apanic.c
@@ -28,9 +28,7 @@
 #include <linux/wakelock.h>
 #include <linux/platform_device.h>
 #include <linux/uaccess.h>
-#include <linux/mtd/mtd.h>
 #include <linux/notifier.h>
-#include <linux/mtd/mtd.h>
 #include <linux/debugfs.h>
 #include <linux/fs.h>
 #include <linux/proc_fs.h>
@@ -55,88 +53,21 @@ struct panic_header {
 };
 
 struct apanic_data {
-	struct mtd_info		*mtd;
 	struct panic_header	curr;
 	void			*bounce;
 	struct proc_dir_entry	*apanic_console;
 	struct proc_dir_entry	*apanic_threads;
+	unsigned int		xfersize;
+	unsigned int		devregistered;
 };
 
 static struct apanic_data drv_ctx;
 static struct work_struct proc_removal_work;
 static DEFINE_MUTEX(drv_mutex);
 
-static unsigned int *apanic_bbt;
-static unsigned int apanic_erase_blocks;
-static unsigned int apanic_good_blocks;
-
-static void set_bb(unsigned int block, unsigned int *bbt)
-{
-	unsigned int flag = 1;
-
-	BUG_ON(block >= apanic_erase_blocks);
-
-	flag = flag << (block%32);
-	apanic_bbt[block/32] |= flag;
-	apanic_good_blocks--;
-}
-
-static unsigned int get_bb(unsigned int block, unsigned int *bbt)
-{
-	unsigned int flag;
-
-	BUG_ON(block >= apanic_erase_blocks);
-
-	flag = 1 << (block%32);
-	return apanic_bbt[block/32] & flag;
-}
-
-static void alloc_bbt(struct mtd_info *mtd, unsigned int *bbt)
-{
-	int bbt_size;
-	apanic_erase_blocks = (mtd->size)>>(mtd->erasesize_shift);
-	bbt_size = (apanic_erase_blocks+32)/32;
-
-	apanic_bbt = kmalloc(bbt_size*4, GFP_KERNEL);
-	memset(apanic_bbt, 0, bbt_size*4);
-	apanic_good_blocks = apanic_erase_blocks;
-}
-static void scan_bbt(struct mtd_info *mtd, unsigned int *bbt)
-{
-	int i;
-
-	for (i = 0; i < apanic_erase_blocks; i++) {
-		if (mtd->block_isbad(mtd, i*mtd->erasesize))
-			set_bb(i, apanic_bbt);
-	}
-}
-
-#define APANIC_INVALID_OFFSET 0xFFFFFFFF
-
-static unsigned int phy_offset(struct mtd_info *mtd, unsigned int offset)
-{
-	unsigned int logic_block = offset>>(mtd->erasesize_shift);
-	unsigned int phy_block;
-	unsigned good_block = 0;
-
-	for (phy_block = 0; phy_block < apanic_erase_blocks; phy_block++) {
-		if (!get_bb(phy_block, apanic_bbt))
-			good_block++;
-		if (good_block == (logic_block + 1))
-			break;
-	}
-
-	if (good_block != (logic_block + 1))
-		return APANIC_INVALID_OFFSET;
-
-	return offset + ((phy_block-logic_block)<<mtd->erasesize_shift);
-}
-
-static void apanic_erase_callback(struct erase_info *done)
-{
-	wait_queue_head_t *wait_q = (wait_queue_head_t *) done->priv;
-	wake_up(wait_q);
-}
+extern int apanic_dev_read(unsigned int addr, size_t *retlen, void *buf);
+extern void apanic_dev_erase(void);
+extern int apanic_dev_write(loff_t to, const u_char *buf);
 
 static int apanic_proc_read(char *buffer, char **start, off_t offset,
 			       int count, int *peof, void *dat)
@@ -148,6 +79,11 @@ static int apanic_proc_read(char *buffer, char **start, off_t offset,
 	off_t page_offset;
 	int rc;
 	size_t len;
+
+	if (!ctx->devregistered) {
+		*peof = 1;
+		return 0;
+	}
 
 	if (!count)
 		return 0;
@@ -174,24 +110,18 @@ static int apanic_proc_read(char *buffer, char **start, off_t offset,
 		return 0;
 	}
 
-	/* We only support reading a maximum of a flash page */
-	if (count > ctx->mtd->writesize)
-		count = ctx->mtd->writesize;
+	if (count > ctx->xfersize)
+		count = ctx->xfersize;
 
-	page_no = (file_offset + offset) / ctx->mtd->writesize;
-	page_offset = (file_offset + offset) % ctx->mtd->writesize;
+	page_no = (file_offset + offset) / ctx->xfersize;
+	page_offset = (file_offset + offset) % ctx->xfersize;
 
+	rc = apanic_dev_read(page_no * ctx->xfersize, &len, ctx->bounce);
 
-	if (phy_offset(ctx->mtd, (page_no * ctx->mtd->writesize))
-		== APANIC_INVALID_OFFSET) {
-		pr_err("apanic: reading an invalid address\n");
+	if (rc) {
 		mutex_unlock(&drv_mutex);
 		return -EINVAL;
 	}
-	rc = ctx->mtd->read(ctx->mtd,
-		phy_offset(ctx->mtd, (page_no * ctx->mtd->writesize)),
-		ctx->mtd->writesize,
-		&len, ctx->bounce);
 
 	if (page_offset)
 		count -= page_offset;
@@ -206,72 +136,13 @@ static int apanic_proc_read(char *buffer, char **start, off_t offset,
 	return count;
 }
 
-static void mtd_panic_erase(void)
-{
-	struct apanic_data *ctx = &drv_ctx;
-	struct erase_info erase;
-	DECLARE_WAITQUEUE(wait, current);
-	wait_queue_head_t wait_q;
-	int rc, i;
-
-	init_waitqueue_head(&wait_q);
-	erase.mtd = ctx->mtd;
-	erase.callback = apanic_erase_callback;
-	erase.len = ctx->mtd->erasesize;
-	erase.priv = (u_long)&wait_q;
-	for (i = 0; i < ctx->mtd->size; i += ctx->mtd->erasesize) {
-		erase.addr = i;
-		set_current_state(TASK_INTERRUPTIBLE);
-		add_wait_queue(&wait_q, &wait);
-
-		if (get_bb(erase.addr>>ctx->mtd->erasesize_shift, apanic_bbt)) {
-			printk(KERN_WARNING
-			       "apanic: Skipping erase of bad "
-			       "block @%llx\n", erase.addr);
-			set_current_state(TASK_RUNNING);
-			remove_wait_queue(&wait_q, &wait);
-			continue;
-		}
-
-		rc = ctx->mtd->erase(ctx->mtd, &erase);
-		if (rc) {
-			set_current_state(TASK_RUNNING);
-			remove_wait_queue(&wait_q, &wait);
-			printk(KERN_ERR
-			       "apanic: Erase of 0x%llx, 0x%llx failed\n",
-			       (unsigned long long) erase.addr,
-			       (unsigned long long) erase.len);
-			if (rc == -EIO) {
-				if (ctx->mtd->block_markbad(ctx->mtd,
-							    erase.addr)) {
-					printk(KERN_ERR
-					       "apanic: Err marking blk bad\n");
-					goto out;
-				}
-				printk(KERN_INFO
-				       "apanic: Marked a bad block"
-				       " @%llx\n", erase.addr);
-				set_bb(erase.addr>>ctx->mtd->erasesize_shift,
-					apanic_bbt);
-				continue;
-			}
-			goto out;
-		}
-		schedule();
-		remove_wait_queue(&wait_q, &wait);
-	}
-	printk(KERN_DEBUG "apanic: %s partition erased\n",
-	       CONFIG_APANIC_PLABEL);
-out:
-	return;
-}
 
 static void apanic_remove_proc_work(struct work_struct *work)
 {
 	struct apanic_data *ctx = &drv_ctx;
 
 	mutex_lock(&drv_mutex);
-	mtd_panic_erase();
+	apanic_dev_erase();
 	memset(&ctx->curr, 0, sizeof(struct panic_header));
 	if (ctx->apanic_console) {
 		remove_proc_entry("apanic_console", NULL);
@@ -287,59 +158,52 @@ static void apanic_remove_proc_work(struct work_struct *work)
 static int apanic_proc_write(struct file *file, const char __user *buffer,
 				unsigned long count, void *data)
 {
-	schedule_work(&proc_removal_work);
+	if (ctx->devregistered)
+		schedule_work(&proc_removal_work);
+
 	return count;
 }
 
-static void mtd_panic_notify_add(struct mtd_info *mtd)
+int apanic_register_device(int regunreg, unsigned int xfersize)
 {
 	struct apanic_data *ctx = &drv_ctx;
 	struct panic_header *hdr = ctx->bounce;
-	size_t len;
-	int rc;
 	int    proc_entry_created = 0;
+	int    rc;
+	size_t len;
 
-	if (strcmp(mtd->name, CONFIG_APANIC_PLABEL))
-		return;
-
-	ctx->mtd = mtd;
-
-	alloc_bbt(mtd, apanic_bbt);
-	scan_bbt(mtd, apanic_bbt);
-
-	if (apanic_good_blocks == 0) {
-		printk(KERN_ERR "apanic: no any good blocks?!\n");
-		goto out_err;
+	if (!regunreg) {
+		drv_ctx.devregistered = 0;
+		return 0;
 	}
 
-	rc = mtd->read(mtd, phy_offset(mtd, 0), mtd->writesize,
-			&len, ctx->bounce);
-	if (rc && rc == -EBADMSG) {
-		printk(KERN_WARNING
-		       "apanic: Bad ECC on block 0 (ignored)\n");
-	} else if (rc && rc != -EUCLEAN) {
-		printk(KERN_ERR "apanic: Error reading block 0 (%d)\n", rc);
-		goto out_err;
+	if ((drv_ctx.bounce = (void *) __get_free_page(GFP_KERNEL)) == NULL) {
+	    printk(KERN_ERR "apanic: Out of memory for bounce buffer\n");
+	    return 1;
 	}
 
-	if (len != mtd->writesize) {
-		printk(KERN_ERR "apanic: Bad read size (%d)\n", rc);
-		goto out_err;
+	drv_ctx.xfersize = xfersize;
+	rc = apanic_dev_read(0, &len, drv_ctx.bounce);
+
+	if (rc) {
+		printk(KERN_ERR "apanic: Error reading panic header (%d)\n", rc);
+		return rc;
 	}
 
-	printk(KERN_INFO "apanic: Bound to mtd partition '%s'\n", mtd->name);
+	drv_ctx.devregistered = 1;
+	hdr = ctx->bounce;
 
 	if (hdr->magic != PANIC_MAGIC) {
 		printk(KERN_INFO "apanic: No panic data available\n");
-		mtd_panic_erase();
-		return;
+		apanic_dev_erase();
+		return 0;
 	}
 
 	if (hdr->version != PHDR_VERSION) {
 		printk(KERN_INFO "apanic: Version mismatch (%d != %d)\n",
 		       hdr->version, PHDR_VERSION);
-		mtd_panic_erase();
-		return;
+		apanic_dev_erase();
+		return 0;
 	}
 
 	memcpy(&ctx->curr, hdr, sizeof(struct panic_header));
@@ -379,64 +243,12 @@ static void mtd_panic_notify_add(struct mtd_info *mtd)
 	}
 
 	if (!proc_entry_created)
-		mtd_panic_erase();
+		apanic_dev_erase();
 
-	return;
-out_err:
-	ctx->mtd = NULL;
+	return 0;
 }
-
-static void mtd_panic_notify_remove(struct mtd_info *mtd)
-{
-	struct apanic_data *ctx = &drv_ctx;
-	if (mtd == ctx->mtd) {
-		ctx->mtd = NULL;
-		printk(KERN_INFO "apanic: Unbound from %s\n", mtd->name);
-	}
-}
-
-static struct mtd_notifier mtd_panic_notifier = {
-	.add	= mtd_panic_notify_add,
-	.remove	= mtd_panic_notify_remove,
-};
 
 static int in_panic = 0;
-
-static int apanic_writeflashpage(struct mtd_info *mtd, loff_t to,
-				 const u_char *buf)
-{
-	int rc;
-	size_t wlen;
-	int panic = in_interrupt() | in_atomic();
-
-	if (panic && !mtd->panic_write) {
-		printk(KERN_EMERG "%s: No panic_write available\n", __func__);
-		return 0;
-	} else if (!panic && !mtd->write) {
-		printk(KERN_EMERG "%s: No write available\n", __func__);
-		return 0;
-	}
-
-	to = phy_offset(mtd, to);
-	if (to == APANIC_INVALID_OFFSET) {
-		printk(KERN_EMERG "apanic: write to invalid address\n");
-		return 0;
-	}
-
-	if (panic)
-		rc = mtd->panic_write(mtd, to, mtd->writesize, &wlen, buf);
-	else
-		rc = mtd->write(mtd, to, mtd->writesize, &wlen, buf);
-
-	if (rc) {
-		printk(KERN_EMERG
-		       "%s: Error writing data to flash (%d)\n",
-		       __func__, rc);
-		return rc;
-	}
-
-	return wlen;
-}
 
 extern int log_buf_copy(char *dest, int idx, int len);
 extern void log_buf_clear(void);
@@ -445,7 +257,7 @@ extern void log_buf_clear(void);
  * Writes the contents of the console to the specified offset in flash.
  * Returns number of bytes written
  */
-static int apanic_write_console(struct mtd_info *mtd, unsigned int off)
+static int apanic_write_console(unsigned int off)
 {
 	struct apanic_data *ctx = &drv_ctx;
 	int saved_oip;
@@ -456,23 +268,24 @@ static int apanic_write_console(struct mtd_info *mtd, unsigned int off)
 	while (!last_chunk) {
 		saved_oip = oops_in_progress;
 		oops_in_progress = 1;
-		rc = log_buf_copy(ctx->bounce, idx, mtd->writesize);
+		rc = log_buf_copy(ctx->bounce, idx, ctx->xfersize);
 		if (rc < 0)
 			break;
 
-		if (rc != mtd->writesize)
+		if (rc != ctx->xfersize)
 			last_chunk = rc;
 
 		oops_in_progress = saved_oip;
 		if (rc <= 0)
 			break;
-		if (rc != mtd->writesize)
-			memset(ctx->bounce + rc, 0, mtd->writesize - rc);
+		if (rc != ctx->xfersize)
+			memset(ctx->bounce + rc, 0, ctx->xfersize - rc);
 
-		rc2 = apanic_writeflashpage(mtd, off, ctx->bounce);
+		rc2 = apanic_dev_write(off, ctx->bounce);
+
 		if (rc2 <= 0) {
 			printk(KERN_EMERG
-			       "apanic: Flash write failed (%d)\n", rc2);
+			       "apanic: Device write failed (%d)\n", rc2);
 			return idx;
 		}
 		if (!last_chunk)
@@ -504,19 +317,20 @@ static int apanic(struct notifier_block *this, unsigned long event,
 #endif
 	touch_softlockup_watchdog();
 
-	if (!ctx->mtd)
+	if (!ctx->devregistered)
 		goto out;
 
 	if (ctx->curr.magic) {
 		printk(KERN_EMERG "Crash partition in use!\n");
 		goto out;
 	}
-	console_offset = ctx->mtd->writesize;
+
+	console_offset = ctx->xfersize;
 
 	/*
 	 * Write out the console
 	 */
-	console_len = apanic_write_console(ctx->mtd, console_offset);
+	console_len = apanic_write_console(console_offset);
 	if (console_len < 0) {
 		printk(KERN_EMERG "Error writing console to panic log! (%d)\n",
 		       console_len);
@@ -527,15 +341,18 @@ static int apanic(struct notifier_block *this, unsigned long event,
 	 * Write out all threads
 	 */
 	threads_offset = ALIGN(console_offset + console_len,
-			       ctx->mtd->writesize);
+			       ctx->xfersize);
 	if (!threads_offset)
-		threads_offset = ctx->mtd->writesize;
+		threads_offset = ctx->xfersize;
 
+#ifdef CONFIG_ANDROID_RAM_CONSOLE
 	ram_console_enable_console(0);
+#endif
 
 	log_buf_clear();
 	show_state_filter(0);
-	threads_len = apanic_write_console(ctx->mtd, threads_offset);
+
+	threads_len = apanic_write_console(threads_offset);
 	if (threads_len < 0) {
 		printk(KERN_EMERG "Error writing threads to panic log! (%d)\n",
 		       threads_len);
@@ -555,7 +372,8 @@ static int apanic(struct notifier_block *this, unsigned long event,
 	hdr->threads_offset = threads_offset;
 	hdr->threads_length = threads_len;
 
-	rc = apanic_writeflashpage(ctx->mtd, 0, ctx->bounce);
+	rc = apanic_dev_write(0, ctx->bounce);
+
 	if (rc <= 0) {
 		printk(KERN_EMERG "apanic: Header write failed (%d)\n",
 		       rc);
@@ -592,14 +410,10 @@ DEFINE_SIMPLE_ATTRIBUTE(panic_dbg_fops, panic_dbg_get, panic_dbg_set, "%llu\n");
 
 int __init apanic_init(void)
 {
-	register_mtd_user(&mtd_panic_notifier);
 	atomic_notifier_chain_register(&panic_notifier_list, &panic_blk);
 	debugfs_create_file("apanic", 0644, NULL, NULL, &panic_dbg_fops);
-	memset(&drv_ctx, 0, sizeof(drv_ctx));
-	drv_ctx.bounce = (void *) __get_free_page(GFP_KERNEL);
 	INIT_WORK(&proc_removal_work, apanic_remove_proc_work);
-	printk(KERN_INFO "Android kernel panic handler initialized (bind=%s)\n",
-	       CONFIG_APANIC_PLABEL);
+	printk(KERN_INFO "Android kernel panic handler initialized\n");
 	return 0;
 }
 
---
+++ drivers/misc/apanic_mtd.c
@@ -0,0 +1,291 @@
+/* drivers/misc/apanic_mtd.c
+ *
+ * Copyright (C) 2009 Google, Inc.
+ * Author: San Mehat <san@android.com>
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/string.h>
+#include <linux/errno.h>
+#include <linux/init.h>
+#include <linux/interrupt.h>
+#include <linux/device.h>
+#include <linux/types.h>
+#include <linux/delay.h>
+#include <linux/sched.h>
+#include <linux/wait.h>
+#include <linux/wakelock.h>
+#include <linux/platform_device.h>
+#include <linux/uaccess.h>
+#include <linux/mtd/mtd.h>
+#include <linux/notifier.h>
+#include <linux/mtd/mtd.h>
+#include <linux/debugfs.h>
+#include <linux/fs.h>
+#include <linux/proc_fs.h>
+#include <linux/mutex.h>
+#include <linux/workqueue.h>
+#include <linux/preempt.h>
+
+static struct apanic_mtd_data {
+	struct mtd_info		*mtd;
+} mtdctx;
+
+static unsigned int *apanic_bbt;
+static unsigned int apanic_erase_blocks;
+static unsigned int apanic_good_blocks;
+
+extern int apanic_register_device(int regunreg, unsigned int xfersize);
+
+static void set_bb(unsigned int block, unsigned int *bbt)
+{
+	unsigned int flag = 1;
+
+	BUG_ON(block >= apanic_erase_blocks);
+
+	flag = flag << (block%32);
+	apanic_bbt[block/32] |= flag;
+	apanic_good_blocks--;
+}
+
+static unsigned int get_bb(unsigned int block, unsigned int *bbt)
+{
+	unsigned int flag;
+
+	BUG_ON(block >= apanic_erase_blocks);
+
+	flag = 1 << (block%32);
+	return apanic_bbt[block/32] & flag;
+}
+
+static void alloc_bbt(struct mtd_info *mtd, unsigned int *bbt)
+{
+	int bbt_size;
+	apanic_erase_blocks = (mtd->size)>>(mtd->erasesize_shift);
+	bbt_size = (apanic_erase_blocks+32)/32;
+
+	apanic_bbt = kmalloc(bbt_size*4, GFP_KERNEL);
+	memset(apanic_bbt, 0, bbt_size*4);
+	apanic_good_blocks = apanic_erase_blocks;
+}
+static void scan_bbt(struct mtd_info *mtd, unsigned int *bbt)
+{
+	int i;
+
+	for (i = 0; i < apanic_erase_blocks; i++) {
+		if (mtd->block_isbad(mtd, i*mtd->erasesize))
+			set_bb(i, apanic_bbt);
+	}
+}
+
+#define APANIC_INVALID_OFFSET 0xFFFFFFFF
+
+static unsigned int phy_offset(struct mtd_info *mtd, unsigned int offset)
+{
+	unsigned int logic_block = offset>>(mtd->erasesize_shift);
+	unsigned int phy_block;
+	unsigned good_block = 0;
+
+	for (phy_block = 0; phy_block < apanic_erase_blocks; phy_block++) {
+		if (!get_bb(phy_block, apanic_bbt))
+			good_block++;
+		if (good_block == (logic_block + 1))
+			break;
+	}
+
+	if (good_block != (logic_block + 1))
+		return APANIC_INVALID_OFFSET;
+
+	return offset + ((phy_block-logic_block)<<mtd->erasesize_shift);
+}
+
+static void apanic_erase_callback(struct erase_info *done)
+{
+	wait_queue_head_t *wait_q = (wait_queue_head_t *) done->priv;
+	wake_up(wait_q);
+}
+
+int apanic_dev_read(unsigned int addr, size_t *retlen, void *buf)
+{
+	unsigned int offset = phy_offset(mtdctx.mtd, addr);
+	int rc;
+
+	if (offset == APANIC_INVALID_OFFSET) {
+		pr_err("apanic mtd: reading an invalid address\n");
+		return -EINVAL;
+	}
+
+	rc = mtdctx.mtd->read(mtdctx.mtd, offset, mtdctx.mtd->writesize,
+			      retlen, buf);
+
+	if (rc == -EBADMSG && offset == 0) {
+		printk(KERN_WARNING
+		       "apanic mtd: Bad ECC on block 0 (ignored)\n");
+	} else if (rc && rc != -EUCLEAN) {
+		printk(KERN_ERR "apanic mtd: Error reading offset %d (%d)\n",
+		       offset, rc);
+		return -EIO;
+	}
+
+	if (*retlen != mtdctx.mtd->writesize) {
+		printk(KERN_ERR "apanic mtd: Bad read length at offset %d"
+		       " (%d, expected %d)\n",
+		       offset, *retlen, mtdctx.mtd->writesize);
+		return -EIO;
+	}
+
+	return rc;
+}
+
+int apanic_dev_write(loff_t to, const u_char *buf)
+{
+	struct mtd_info *mtd = mtdctx.mtd;
+	int rc;
+	size_t wlen;
+	int panic = in_interrupt() | in_atomic();
+
+	if (panic && !mtd->panic_write) {
+		printk(KERN_EMERG "%s: No panic_write available\n", __func__);
+		return 0;
+	} else if (!panic && !mtd->write) {
+		printk(KERN_EMERG "%s: No write available\n", __func__);
+		return 0;
+	}
+
+	to = phy_offset(mtd, to);
+	if (to == APANIC_INVALID_OFFSET) {
+		printk(KERN_EMERG "apanic: write to invalid address\n");
+		return 0;
+	}
+
+	if (panic)
+		rc = mtd->panic_write(mtd, to, mtd->writesize, &wlen, buf);
+	else
+		rc = mtd->write(mtd, to, mtd->writesize, &wlen, buf);
+
+	if (rc) {
+		printk(KERN_EMERG
+		       "%s: Error writing data to flash (%d)\n",
+		       __func__, rc);
+		return rc;
+	}
+
+	return wlen;
+}
+
+void apanic_dev_erase(void)
+{
+	struct erase_info erase;
+	DECLARE_WAITQUEUE(wait, current);
+	wait_queue_head_t wait_q;
+	int rc, i;
+
+	init_waitqueue_head(&wait_q);
+	erase.mtd = mtdctx.mtd;
+	erase.callback = apanic_erase_callback;
+	erase.len = mtdctx.mtd->erasesize;
+	erase.priv = (u_long)&wait_q;
+	for (i = 0; i < mtdctx.mtd->size; i += mtdctx.mtd->erasesize) {
+		erase.addr = i;
+		set_current_state(TASK_INTERRUPTIBLE);
+		add_wait_queue(&wait_q, &wait);
+
+		if (get_bb(erase.addr>>mtdctx.mtd->erasesize_shift, apanic_bbt)) {
+			printk(KERN_WARNING
+			       "apanic mtd: Skipping erase of bad "
+			       "block @%llx\n", erase.addr);
+			set_current_state(TASK_RUNNING);
+			remove_wait_queue(&wait_q, &wait);
+			continue;
+		}
+
+		rc = mtdctx.mtd->erase(mtdctx.mtd, &erase);
+		if (rc) {
+			set_current_state(TASK_RUNNING);
+			remove_wait_queue(&wait_q, &wait);
+			printk(KERN_ERR
+			       "apanic mtd: Erase of 0x%llx, 0x%llx failed\n",
+			       (unsigned long long) erase.addr,
+			       (unsigned long long) erase.len);
+			if (rc == -EIO) {
+				if (mtdctx.mtd->block_markbad(mtdctx.mtd,
+							    erase.addr)) {
+					printk(KERN_ERR
+					       "apanic mtd: Err marking blk bad\n");
+					goto out;
+				}
+				printk(KERN_INFO
+				       "apanic mtd: Marked a bad block"
+				       " @%llx\n", erase.addr);
+				set_bb(erase.addr>>mtdctx.mtd->erasesize_shift,
+					apanic_bbt);
+				continue;
+			}
+			goto out;
+		}
+		schedule();
+		remove_wait_queue(&wait_q, &wait);
+	}
+	printk(KERN_DEBUG "apanic mtd: %s partition erased\n",
+	       CONFIG_APANIC_PLABEL);
+out:
+	return;
+}
+
+static void mtd_panic_notify_add(struct mtd_info *mtd)
+{
+	if (strcmp(mtd->name, CONFIG_APANIC_PLABEL))
+		return;
+
+	mtdctx.mtd = mtd;
+	printk(KERN_INFO "apanic mtd: Bound to partition '%s'\n", mtd->name);
+
+	alloc_bbt(mtd, apanic_bbt);
+	scan_bbt(mtd, apanic_bbt);
+
+	if (apanic_good_blocks == 0) {
+		printk(KERN_ERR "apanic mtd: no any good blocks?!\n");
+		goto out_err;
+	}
+
+	if (apanic_register_device(1, mtd->writesize))
+		goto out_err;
+
+	return;
+out_err:
+	mtdctx.mtd = NULL;
+}
+
+static void mtd_panic_notify_remove(struct mtd_info *mtd)
+{
+	if (mtd == mtdctx.mtd) {
+		mtdctx.mtd = NULL;
+		apanic_register_device(0, mtd->writesize);
+		printk(KERN_INFO "apanic mtd: Unbound from %s\n", mtd->name);
+	}
+}
+
+static struct mtd_notifier mtd_panic_notifier = {
+	.add	= mtd_panic_notify_add,
+	.remove	= mtd_panic_notify_remove,
+};
+
+int __init apanic_mtd_init(void)
+{
+	memset(&mtdctx, 0, sizeof(mtdctx));
+	register_mtd_user(&mtd_panic_notifier);
+	return 0;
+}
+
+module_init(apanic_mtd_init);
