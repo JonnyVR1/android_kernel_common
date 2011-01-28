6db1a2006dc1921b068f08c63d967fd199cfc73e
Ashish Sharma <ashishsharma@google.com>
2011-01-27 17:13:18 -0800

    net: Add UDP stats and pkt count to uid_stat
    Change-Id: If95374a6dbde66720a3c74b5b50cc7e774bbbbf8
    Signed-off-by: Ashish Sharma <ashishsharma@google.com>

--- net/ipv4/udp.c
+++ net/ipv4/udp.c
@@ -100,6 +100,7 @@
 #include <linux/skbuff.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <linux/uid_stat.h>
 #include <net/net_namespace.h>
 #include <net/icmp.h>
 #include <net/route.h>
@@ -962,8 +963,10 @@ out:
 	ip_rt_put(rt);
 	if (free)
 		kfree(ipc.opt);
-	if (!err)
+	if (!err) {
+		uid_stat_udp_snd(current_uid(), len);
 		return len;
+	}
 	/*
 	 * ENOBUFS = no kernel mem, SOCK_NOSPACE = no sndbuf space.  Reporting
 	 * ENOBUFS might not be good (it's not tunable per se), but otherwise
@@ -1122,7 +1125,7 @@ int udp_recvmsg(struct kiocb *iocb, struct sock *sk, struct msghdr *msg,
 	struct sk_buff *skb;
 	unsigned int ulen;
 	int peeked;
-	int err;
+	int err = 0;
 	int is_udplite = IS_UDPLITE(sk);
 	bool slow;
 
@@ -1196,6 +1199,8 @@ try_again:
 out_free:
 	skb_free_datagram_locked(sk, skb);
 out:
+	if (err > 0)
+		uid_stat_udp_rcv(current_uid(), err);
 	return err;
 
 csum_copy_err:
