47df4f25aa90dafdee99bf2024549d043b67d1ce
Todd Poynor <toddpoynor@google.com>
2011-06-15 17:21:57 -0700

    Move x86_64 idle notifiers to generic
    Move the x86_64 idle notifiers originally by Andi Kleen and Venkatesh
    Pallipadi to generic.
    
    Change-Id: Idf29cda15be151f494ff245933c12462643388d5
    Signed-off-by: Todd Poynor <toddpoynor@google.com>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -594,3 +594,17 @@ void init_cpu_online(const struct cpumask *src)
 {
 	cpumask_copy(to_cpumask(cpu_online_bits), src);
 }
+
+ATOMIC_NOTIFIER_HEAD(idle_notifier);
+
+void idle_notifier_register(struct notifier_block *n)
+{
+	atomic_notifier_chain_register(&idle_notifier, n);
+}
+EXPORT_SYMBOL_GPL(idle_notifier_register);
+
+void idle_notifier_unregister(struct notifier_block *n)
+{
+	atomic_notifier_chain_unregister(&idle_notifier, n);
+}
+EXPORT_SYMBOL_GPL(idle_notifier_unregister);
