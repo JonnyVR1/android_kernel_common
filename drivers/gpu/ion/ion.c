ad2eb5168687fc0b4a01764f40708ac1ca77d4cf
Rebecca Schultz Zavin <rebecca@android.com>
2012-06-07 16:36:44 -0700

    gpu: ion: Add ion_page_pool.
    This patch adds a new utility heaps can use to manage
    memory.  In the past we have found it can be very
    expensive to manage the caches when allocating memory,
    but it is imposible to know whether a previous user of a
    given memory allocation had a cached mapping. This patch
    adds the ability to store a pool of pages that were
    previously used uncached so that cache maintenance
    only need be done when growing this pool.  The pool also
    contains a shrinker so memory from the pool can be
    recovered in low memory conditions.
    
    Change-Id: I686fd7d192060fd32d34ef66471f62b7829006ee
    Signed-off-by: Rebecca Schultz Zavin <rebecca@android.com>

--- drivers/gpu/ion/ion.c
+++ drivers/gpu/ion/ion.c
@@ -1232,6 +1232,10 @@ static int ion_debug_heap_show(struct seq_file *s, void *unused)
 	seq_printf(s, "%16.s %16u\n", "total orphaned",
 		   total_orphaned_size);
 	seq_printf(s, "%16.s %16u\n", "total ", total_size);
+	seq_printf(s, "----------------------------------------------------\n");
+
+	if (heap->debug_show)
+		heap->debug_show(heap, s, unused);
 
 	return 0;
 }
