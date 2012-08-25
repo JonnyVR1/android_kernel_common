4e8b3d47d2e0dbc8e2b3f13582a9f440fbac9990
Colin Cross <ccross@android.com>
2012-08-25 15:29:24 -0700

    ARM: stacktrace: harden FP stacktraces against invalid stacks
    Dumping stacktraces is currently disabled in ARM SMP for all tasks
    except the current task due to the worry that the task may be running
    on another CPU and that the unwinder may be unstable when presented
    with a stack that is being modified.
    
    Unwinding with CONFIG_FRAME_POINTER is fairly simple compared to
    when CONFIG_ARM_UNWIND is set.  The next frame's FP and SP registers
    are read from the stack and can be validated against the current
    values to ensure that they do not leave the stack and make progress
    towards the upper end of the stack.  This guarantees that accesses
    do not fault and that execution is bounded.
    
    Add additional validations to the version of unwind_frame used when
    CONFIG_FRAME_POINTER=y:
    
    Verify that the stack is in a mapped region of kernel memory.
    Fixes crashes seen in unwind_frame on real systems, although
    stack corruption caused by memory instability is likely the cause
    of the invalid sp and fp values.
    
    Fix address comparison to catch fp >= 0xfffffffc correctly.
    Fixes crash reported by Todd Poynor.
    
    Ensure the stack pointer moves to a higher address between each
    frame to make sure repeated calls to unwind_frame can't loop
    forever.
    
    Includes ideas from Dave Martin.
    
    Change-Id: I949eae5a957c8826a289e880180c06985dea6f5d
    Signed-off-by: Colin Cross <ccross@android.com>

--- arch/arm/include/asm/stacktrace.h
+++ arch/arm/include/asm/stacktrace.h
@@ -12,4 +12,8 @@ extern int unwind_frame(struct stackframe *frame);
 extern void walk_stackframe(struct stackframe *frame,
 			    int (*fn)(struct stackframe *, void *), void *data);
 
+bool sp_addr_valid(unsigned long sp);
+bool addr_in_stack(unsigned long orig_sp, unsigned long vsp);
+bool sp_in_stack(unsigned long orig_sp, unsigned long vsp);
+
 #endif	/* __ASM_STACKTRACE_H */
