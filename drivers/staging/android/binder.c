4dd548c4c962d0d0a7232b645a8a23bdb7df7af2
Stephen Smalley <sds@tycho.nsa.gov>
2012-11-05 08:15:34 -0500

    BACKPORT: Add security hooks to binder and implement the hooks for SELinux.
    (cherry pick from commit c76b9f83bfc0551c065229c13c785675d6da9825)
    
    Conflict, this is an innoculation to move
    security_binder_set_context_mgr to correct location.
    
    Add security hooks to the binder and implement the hooks for SELinux.
    The security hooks enable security modules such as SELinux to implement
    controls over binder IPC.  The security hooks include support for
    controlling what process can become the binder context manager
    (binder_set_context_mgr), controlling the ability of a process
    to invoke a binder transaction/IPC to another process (binder_transaction),
    controlling the ability a process to transfer a binder reference to
    another process (binder_transfer_binder), and controlling the ability
    of a process to transfer an open file to another process (binder_transfer_file).
    
    This support is used by SE Android, http://selinuxproject.org/page/SEAndroid.
    
    Signed-off-by: Stephen Smalley <sds@tycho.nsa.gov>
    Bug: 30141999
    Change-Id: I34266b66320b6a3df9ac01833d7f94daf742920e

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -1559,7 +1559,7 @@ static void binder_transaction(struct binder_proc *proc,
 				return_error = BR_FAILED_REPLY;
 				goto err_binder_get_ref_for_node_failed;
 			}
-	if (security_binder_transfer_binder(proc->tsk, target_proc->tsk)) {
+			if (security_binder_transfer_binder(proc->tsk, target_proc->tsk)) {
 				return_error = BR_FAILED_REPLY;
 				goto err_binder_get_ref_for_node_failed;
 			}
@@ -2703,6 +2703,9 @@ static int binder_ioctl_set_ctx_mgr(struct file *filp)
 		ret = -EBUSY;
 		goto out;
 	}
+	ret = security_binder_set_context_mgr(proc->tsk);
+	if (ret < 0)
+		goto out;
 	if (uid_valid(binder_context_mgr_uid)) {
 		if (!uid_eq(binder_context_mgr_uid, curr_euid)) {
 			pr_err("BINDER_SET_CONTEXT_MGR bad uid %d != %d\n",
@@ -2767,9 +2770,6 @@ static long binder_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 	case BINDER_SET_CONTEXT_MGR:
 		ret = binder_ioctl_set_ctx_mgr(filp);
 		if (ret)
-			goto err;
-		ret = security_binder_set_context_mgr(proc->tsk);
-		if (ret < 0)
 			goto err;
 		break;
 	case BINDER_THREAD_EXIT:
