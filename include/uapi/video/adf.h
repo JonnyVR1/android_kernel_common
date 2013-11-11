11c12bd9339a8201264eda6ee4a27cbeb35626e9
Greg Hackmann <ghackmann@google.com>
2013-11-11 14:31:12 -0800

    video: adf: fix ADF_MAX_ATTACHMENTS declaration
    Userspace-facing ADF_MAX_ATTACHMENTS must be in terms of
    userspace-facing struct adf_attachment_config
    
    Change-Id: Iaaddcd6366f13b3e52eb3911efcfff8a61e0b225
    Signed-off-by: Greg Hackmann <ghackmann@google.com>

--- include/uapi/video/adf.h
+++ include/uapi/video/adf.h
@@ -233,7 +233,7 @@ struct adf_device_data {
 	size_t custom_data_size;
 	void __user *custom_data;
 };
-#define ADF_MAX_ATTACHMENTS (PAGE_SIZE / sizeof(struct adf_attachment))
+#define ADF_MAX_ATTACHMENTS (PAGE_SIZE / sizeof(struct adf_attachment_config))
 
 /**
  * struct adf_device_data - describes a display interface
