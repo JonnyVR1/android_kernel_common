2a5750fb56b6d708f0aecd29d89f867881289e79
Peter (Kyoungchae) Oh <poh@broadcom.com>
2014-05-12 12:43:03 -0700

    usb: free up composite gadget string ids on unbind
    There are only 254 USB composite gadget string_ids available.
    When switching gadget mode such as mtp and acm repeatedly,
    they will be exhausted.
    
    This bug has been brought up since android composite driver
    introduced a way to switch gadget modes while the composite
    driver is still holding its bind.
    
    Fix this by reset next_string_id and clean up gstrings when
    android gadgets are disabled. Also by removing the condition
    comparing gadgets' string id to 0 because gadget string id
    has to be re-assigned whenever the string count is reset.
    
    The codes removed the condition check will work as the same
    as before they have changed if the gadgets are used by other
    composite drivers other than android since all of them call
    bind only once and never unbind it hence no side effects
    considered.
    
    b/15139693
    
    Change-Id: I23733e425ec67fbc06bf99831b757d3264248f91
    Reported-by: Chris Brand <chris.brand@broadcom.com>
    Signed-off-by: Peter (Kyoungchae) Oh <poh@broadcom.com>
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -97,6 +97,7 @@ struct android_dev {
 	bool sw_connected;
 	struct work_struct work;
 	char ffs_aliases[256];
+	unsigned short ffs_string_ids;
 };
 
 static struct class *android_class;
@@ -150,6 +151,20 @@ static struct usb_configuration android_config_driver = {
 	.MaxPower	= 500, /* 500ma */
 };
 
+static void android_gstring_cleanup(struct android_dev *dev)
+{
+	struct usb_composite_dev *cdev = dev->cdev;
+	struct usb_gadget_string_container *uc, *tmp;
+
+	list_for_each_entry_safe(uc, tmp, &cdev->gstrings, list) {
+		list_del(&uc->list);
+		kfree(uc);
+	}
+	/* reserve unfreed string ids */
+	cdev->next_string_id = ARRAY_SIZE(strings_dev) +
+		dev->ffs_string_ids - 1;
+}
+
 static void android_work(struct work_struct *data)
 {
 	struct android_dev *dev = container_of(data, struct android_dev, work);
@@ -200,6 +215,7 @@ static void android_disable(struct android_dev *dev)
 		/* Cancel pending control requests */
 		usb_ep_dequeue(cdev->gadget->ep0, cdev->req);
 		usb_remove_config(cdev, &android_config_driver);
+		android_gstring_cleanup(dev);
 	}
 }
 
@@ -325,6 +341,7 @@ static int functionfs_ready_callback(struct ffs_data *ffs)
 
 	config->data = ffs;
 	config->opened = true;
+	dev->ffs_string_ids = ffs->strings_count;
 
 	if (config->enabled)
 		android_enable(dev);
@@ -348,6 +365,7 @@ static void functionfs_closed_callback(struct ffs_data *ffs)
 	config->data = NULL;
 
 	functionfs_unbind(ffs);
+	dev->ffs_string_ids = 0;
 
 	mutex_unlock(&dev->mutex);
 }
