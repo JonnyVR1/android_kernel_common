9dc1e64c88736bc01e542d32b34a39f57f64e406
Mark Salyzyn <salyzyn@google.com>
2014-12-08 15:06:52 -0800

    pstore: add pmsg
    A secured user-space accessible pstore object. Writes
    to /dev/pmsg0 are appended to the buffer, on reboot
    the persistent contents are available in
    /sys/fs/pstore/pmsg-ramoops-[ID].
    
    One possible use is syslogd, or other daemon, can
    write messages, then on reboot provides a means to
    triage user-space activities leading up to a panic
    as a companion to the pstore dmesg or console logs.
    
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    Change-Id: I82a2a9a989d7583c5fcb65ff520027dc3a034a4c

--- fs/pstore/inode.c
+++ fs/pstore/inode.c
@@ -324,6 +324,9 @@ int pstore_mkfile(enum pstore_type_id type, char *psname, u64 id, int count,
 	case PSTORE_TYPE_MCE:
 		snprintf(name, sizeof(name), "mce-%s-%lld", psname, id);
 		break;
+	case PSTORE_TYPE_PMSG:
+		snprintf(name, sizeof(name), "pmsg-%s-%lld", psname, id);
+		break;
 	case PSTORE_TYPE_UNKNOWN:
 		snprintf(name, sizeof(name), "unknown-%s-%lld", psname, id);
 		break;
