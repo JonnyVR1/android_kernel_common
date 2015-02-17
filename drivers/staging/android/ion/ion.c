9545a2f3ca15c55f9c1e67380904d4c03a88b4e7
Prasad Sodagudi <psodagud@codeaurora.org>
2015-02-17 17:35:20 +0530

    ion: restructure ion clients debugfs entry
    ION client can get created and destroyed dynamically.
    Client data struct may not be in proper state if one core is reading
    client information and other core is destroying the client. Because
    in ion_debug_client_show() function, there is no way to check whether
    the client is part of ION clients rb_tree or not. Also there is
    no synchronization between ion_debug_client_show() and
    ion_client_destroy(), that leaves client data structures in
    uncertain state and which may lead to system crash.
    
    Signed-off-by: Prasad Sodagudi <psodagud@codeaurora.org>

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -674,9 +674,16 @@ void ion_unmap_kernel(struct ion_client *client, struct ion_handle *handle)
 }
 EXPORT_SYMBOL(ion_unmap_kernel);
 
-static int ion_debug_client_show(struct seq_file *s, void *unused)
+static void *ion_client_seq_start(struct seq_file *s, loff_t *pos)
 {
-	struct ion_client *client = s->private;
+	struct ion_device *dev = (struct ion_device *)s->private;
+
+	down_write(&dev->lock);
+	return *pos < 1 ? (void *)1 : NULL;
+}
+
+static void ion_debug_client_show(struct seq_file *s, struct ion_client *client)
+{
 	struct rb_node *n;
 	size_t sizes[ION_NUM_HEAP_IDS] = {0};
 	const char *names[ION_NUM_HEAP_IDS] = {NULL};
@@ -700,19 +707,58 @@ static int ion_debug_client_show(struct seq_file *s, void *unused)
 			continue;
 		seq_printf(s, "%16.16s: %16zu\n", names[i], sizes[i]);
 	}
+}
+
+static int ion_client_seq_show(struct seq_file *s, void *v)
+{
+	struct ion_device *dev = (struct ion_device *)s->private;
+	struct rb_node *cnode;
+
+	for (cnode = rb_first(&dev->clients); cnode; cnode = rb_next(cnode)) {
+		struct ion_client *client = rb_entry(cnode,
+					struct ion_client, node);
+		ion_debug_client_show(s, client);
+	}
 	return 0;
 }
 
+static void *ion_client_seq_next(struct seq_file *s, void *v, loff_t *pos)
+{
+	++*pos;
+	return NULL;
+}
+
+static void ion_client_seq_stop(struct seq_file *s, void *v)
+{
+       struct ion_device *dev = (struct ion_device *)s->private;
+
+       up_write(&dev->lock);
+}
+
+static const struct seq_operations ion_seq_ops = {
+	.start = ion_client_seq_start,
+	.next = ion_client_seq_next,
+	.stop = ion_client_seq_stop,
+	.show = ion_client_seq_show,
+};
+
 static int ion_debug_client_open(struct inode *inode, struct file *file)
 {
-	return single_open(file, ion_debug_client_show, inode->i_private);
+	int ret = seq_open(file, &ion_seq_ops);
+
+	if (!ret) {
+		struct seq_file *s = (struct seq_file *)file->private_data;
+		s->private = inode->i_private;
+	}
+
+	return ret;
 }
 
 static const struct file_operations debug_client_fops = {
 	.open = ion_debug_client_open,
 	.read = seq_read,
 	.llseek = seq_lseek,
-	.release = single_release,
+	.release = seq_release,
 };
 
 static int ion_get_client_serial(const struct rb_root *root,
@@ -795,16 +841,6 @@ struct ion_client *ion_client_create(struct ion_device *dev,
 	rb_link_node(&client->node, parent, p);
 	rb_insert_color(&client->node, &dev->clients);
 
-	client->debug_root = debugfs_create_file(client->display_name, 0664,
-						dev->clients_debug_root,
-						client, &debug_client_fops);
-	if (!client->debug_root) {
-		char buf[256], *path;
-		path = dentry_path(dev->clients_debug_root, buf, 256);
-		pr_err("Failed to create client debugfs at %s/%s\n",
-			path, client->display_name);
-	}
-
 	up_write(&dev->lock);
 
 	return client;
@@ -838,7 +874,6 @@ void ion_client_destroy(struct ion_client *client)
 	if (client->task)
 		put_task_struct(client->task);
 	rb_erase(&client->node, &dev->clients);
-	debugfs_remove_recursive(client->debug_root);
 	up_write(&dev->lock);
 
 	kfree(client->display_name);
@@ -1583,10 +1618,10 @@ struct ion_device *ion_device_create(long (*custom_ioctl)
 		pr_err("ion: failed to create debugfs heaps directory.\n");
 		goto debugfs_done;
 	}
-	idev->clients_debug_root = debugfs_create_dir("clients",
-						idev->debug_root);
+	idev->clients_debug_root = debugfs_create_file("clients", 0664,
+			idev->debug_root, idev, &debug_client_fops);
 	if (!idev->clients_debug_root)
-		pr_err("ion: failed to create debugfs clients directory.\n");
+		pr_err("ion: failed to create debugfs clients file.\n");
 
 debugfs_done:
 
