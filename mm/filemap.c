273f940bf97e990ea15b5dfdd28c6540ae6bbfba
Daniel Campello <campello@google.com>
2015-07-20 10:37:56 -0700

    Page cache miss tracing using ftrace on mm/filemap
    This patch includes two trace events on generic_perform_write and
    do_generic_file_read to check on the address_space mapping for the
    pages to be accessed by the request.
    
    Change-Id: I64c1cebe9edaa5a6aca24583b6a696824a10358a
    Signed-off-by: Daniel Campello <campello@google.com>

--- mm/filemap.c
+++ mm/filemap.c
@@ -1477,6 +1477,8 @@ static ssize_t do_generic_file_read(struct file *filp, loff_t *ppos,
 	unsigned int prev_offset;
 	int error = 0;
 
+	trace_mm_filemap_do_generic_file_read(filp, *ppos, desc->count, 1);
+
 	index = *ppos >> PAGE_CACHE_SHIFT;
 	prev_index = ra->prev_pos >> PAGE_CACHE_SHIFT;
 	prev_offset = ra->prev_pos & (PAGE_CACHE_SIZE-1);
@@ -2460,6 +2462,8 @@ ssize_t generic_perform_write(struct file *file,
 	long status = 0;
 	ssize_t written = 0;
 	unsigned int flags = 0;
+
+	trace_mm_filemap_generic_perform_write(file, pos, iov_iter_count(i), 0);
 
 	/*
 	 * Copies from kernel address space cannot fail (NFSD is a big user).
